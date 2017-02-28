e521f7adf867a8ed8a2b3790fb7e3acf7d6e0d73
HaMzADeveloper <hamzadeveloper@hotmail.com>
2017-02-27 21:33:44 -0800

    Create skeleton.java

---
+++ MonsterWars/src/me/iHDeveloper/entity/skeleton/skeleton.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.iHDeveloper.entity.skeleton;
+
+import net.minecraft.server.v1_8_R1.Zombie;
+import net.minecraft.server.v1_8_R1.EntityLiving;
+import net.minecraft.server.v1_8_R1.EntityZombie;
+import net.minecraft.server.v1_8_R1.World;
+
+/**
+ *
+ * @author Admin
+ */
+public class Skeleton extends Monster{
+    
+    private Skeleton skeleton;
+    private EntityZombie entity;
+    private EntityLiving living;
+    private Location location;
+    
+    // Setup Entity
+    public Skeleton(Location location) {
+        // TODO create entity is a skeleton
+    }
+    
+    
+    
+}
