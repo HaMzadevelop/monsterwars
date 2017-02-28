c27ef84bf12c0f045d0b14f0c48cb4999932a2a1
HaMzADeveloper <hamzadeveloper@hotmail.com>
2017-02-27 22:01:51 -0800

    Update Zombie.java

--- MonsterWars/src/me/iHDeveloper/entity/zombie/Zombie.java
+++ MonsterWars/src/me/iHDeveloper/entity/zombie/Zombie.java
@@ -7,8 +7,6 @@ package me.iHDeveloper.entity.zombie;
 
 import net.minecraft.server.v1_8_R1.Zombie;
 import net.minecraft.server.v1_8_R1.EntityLiving;
-import net.minecraft.server.v1_8_R1.EntityZombie;
-import net.minecraft.server.v1_8_R1.World;
 
 /**
  *
@@ -16,14 +14,17 @@ import net.minecraft.server.v1_8_R1.World;
  */
 public class Zombie extends Monster{
     
-    private Zombie zombie;
-    private EntityZombie entity;
+    private org.bukkit.entity.Zombie entity;
     private EntityLiving living;
     private Location location;
     
     // Setup Entity
     public Zombie(Location location) {
-        // TODO create a zombie 
+        entity = (org.bukkit.entity.Zombie)location.getWorld().spawnEntity(location, EntityType.ZOMBIE);
+    }
+    
+    public void setMaxHealth(int maxHealth){
+        entity.setMaxHealth(maxHealth);
     }
     
     
