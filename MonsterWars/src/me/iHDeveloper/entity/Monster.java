0fd5bc0f87ccb4887cb2870ca7c600a5e1fe4a88
HaMzADeveloper <hamzadeveloper@hotmail.com>
2017-02-27 22:08:29 -0800

    Update Monster.java

--- MonsterWars/src/me/iHDeveloper/entity/Monster.java
+++ MonsterWars/src/me/iHDeveloper/entity/Monster.java
@@ -1,5 +1,13 @@
 package me.iHDeveloper.entity;
 
 public class Monster{
-  // Only for teams
+  
+  public static Monster[] getMonsters(){
+    List<Monster> monsters = new ArrayList<Monster>();
+    monsters.add(new Zombie());
+    monsters.add(new Skeleton());
+    return monsters.toArray(new Monster[0]);
+  }
+  
+  // Only for monsters to
 }
